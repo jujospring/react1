4a7450e1d9bad4d126927494e60de28acf970e34
jujospring <jujospring@gmail.com>
2024-05-24 16:40:47 -0400

    Quick name update

--- todo-list/src/NavBar.jsx
+++ todo-list/src/NavBar.jsx
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function ButtonAppBar() {
+export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
